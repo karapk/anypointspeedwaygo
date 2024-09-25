23446ccc581a73fa23007ee2f8d5f27e23b97fa8
Philip Karanja <kpk.my.mailings@gmail.com>
2024-09-25 14:42:34 -0500

    vercel deploy test

--- main.go
+++ main.go
@@ -1,73 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-type login int
-type welcome int
-
-func (l login) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	// if r.Method == "GET"{
-	// 	fmt.Fprintln(w, "Using GET for login endpoint")
-	// }
-	if r.Method == "POST"{
-		fmt.Fprintln(w, "Using POST for login endpoint")
-	}
-	switch r.Method{
-	case "GET":
-		fmt.Fprintln(w, "Using GET for easier login endpoint")
-	case "POST":
-		fmt.Fprintln(w, "Using POST for easier login endpoint")
-	}
-	fmt.Fprintln(w, "on log in page")
-}
-
-func (wl welcome) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "on Welcome page")
-}
-
-
-
-
-func mylogin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `
-	<html>
-		<head>	
-		</head>
-		<body>
-			<h1>'Log in Anypoint racing--GO! 🚗💨'</h1>
-		</body>
-	</html>
-	`)
-}
-
-func mywelcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `
-	<html>
-		<head>	
-		</head>
-		<body>
-			<h1>'Welcome to Anypoint racing--GO! 🚗💨'</h1>
-		</body>
-	</html>
-	`)
-}
-
 func main() {
-	//accepts handler accepts patterna dn function signature but not same name
-	// http.HandleFunc("/login", mylogin)
-	// http.HandleFunc("/welcome", mywelcome)
-	// accepts handler interface of type http.Handler
-	//accepts pattern and handler
-	// http.Handle("/login", http.HandlerFunc(mylogin))
-	// http.Handle("/welcome", http.HandlerFunc(mywelcome))
-	var i login
-	var j welcome
-	http.Handle("/login", i)
-	http.Handle("/welcome", j)
-	fmt.Println("Listening on port 3001")
-	// var foo MyServerType
-	http.ListenAndServe("localhost:3001", nil)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to Anypoint racing! 🚗💨")
+	})
+	e.Logger.Print("Listening to port 4000")
+	e.Logger.Fatal(e.Start(":4000"))
 }
