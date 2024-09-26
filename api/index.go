6651d60f809fc18468987f40abd0313a97b94770
Philip Karanja <kpk.my.mailings@gmail.com>
2024-09-26 17:00:23 -0500

    racer on vercel

--- api/index.go
+++ api/index.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	."github.com/tbxark/g4vercel"
+
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -28,16 +29,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		})
 	})
 	server.GET("/hello", func(context *Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, H{
-				"message": "name not found",
-			})
-		} else {
-			context.JSON(200, H{
-				"data": fmt.Sprintf("Hello %s!", name),
-			})
-		}
+		context.String(200, "Welcome to Anypoint Racing! 🚗💨")
 	})
 	server.GET("/user/:id", func(context *Context) {
 		context.JSON(400, H{
