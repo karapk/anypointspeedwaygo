71a83a84bdc919bf7ac075d26a998c49e0498cfd
Philip Karanja <kpk.my.mailings@gmail.com>
2024-09-26 18:24:26 -0500

    racer deployment

--- api/index.go
+++ api/index.go
@@ -1,50 +1,108 @@
-
 package handler
 
 import (
-	"fmt"
 	"net/http"
+	"encoding/json"
 
-	."github.com/tbxark/g4vercel"
-
+	"github.com/google/uuid"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
-	server.Use(Recovery(func(err interface{}, c *Context) {
-		if httpError, ok := err.(HttpError); ok {
-			c.JSON(httpError.Status, H{
-				"message": httpError.Error(),
-			})
-		} else {
-			message := fmt.Sprintf("%s", err)
-			c.JSON(500, H{
-				"message": message,
-			})
-		}
-	}))
-	server.GET("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "OK",
-		})
-	})
-	server.GET("/hello", func(context *Context) {
-		context.String(200, "Welcome to Anypoint Racing! 🚗💨")
-	})
-	server.GET("/user/:id", func(context *Context) {
-		context.JSON(400, H{
-			"data": H{
-				"id": context.Param("id"),
-			},
-		})
-	})
-	server.GET("/long/long/long/path/*test", func(context *Context) {
-		context.JSON(200, H{
-			"data": H{
-				"url": context.Path,
-			},
-		})
-	})
-	server.Handle(w, r)
+
+var myDb = struct {
+	races map[string][]string
+}{
+	races: make(map[string][]string),
 }
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handleGet(w, r)
+	case http.MethodPost:
+		handlePost(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+\
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to Anypoint Racing! 🏎️"))
+		return
+	}
+	http.Error(w, "Not Found", http.StatusNotFound)
+}
+
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/races":
+		startRace(w, r)
+	case "/races/":
+		
+		id := r.URL.Path[len("/races/"):]
+		completeLap(w, r, id)
+	default:
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
+}
+
+
+func startRace(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Token string `json:"token"`
+	}
+
+	
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	raceId := uuid.New().String()
+	myDb.races[raceId] = []string{body.Token} // Initialize with the starting token
+
+	toSend := map[string]interface{}{
+		"id":      raceId,
+		"racerId": "2532c7d5-511b-466a-a8b7-bb6c797efa36",
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(toSend)
+}
+
+
+func completeLap(w http.ResponseWriter, r *http.Request, raceId string) {
+	var receivedToken string
+
+	
+	if err := json.NewDecoder(r.Body).Decode(&receivedToken); err != nil {
+		http.Error(w, "Invalid token", http.StatusBadRequest)
+		return
+	}
+
+	tokens, ok := myDb.races[raceId]
+	if !ok {
+		http.Error(w, "Race ID not found", http.StatusNotFound)
+		return
+	}
+
+	tokens = append(tokens, receivedToken) 
+	myDb.races[raceId] = tokens
+
+	if len(tokens) < 2 {
+		http.Error(w, "No valid token to return", http.StatusBadRequest)
+		return
+	}
+
+	previousToken := tokens[len(tokens)-2]
+	toSend := map[string]interface{}{
+		"token":   previousToken,
+		"racerId": "2532c7d5-511b-466a-a8b7-bb6c797efa36",
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(toSend)
+}
